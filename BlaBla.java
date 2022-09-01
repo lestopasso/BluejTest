75b1c40a8f90c389495a9664fc8b229008c12bd7
Luigi <lestopasso@gmail.com>
2022-09-01 15:38:20 +0200

    Condivisione del progetto iniziata...

---
+++ BlaBla.java
@@ -0,0 +1,34 @@
+
+/**
+ * Aggiungi qui una descrizione della classe BlaBla
+ * 
+ * @author (il tuo nome) 
+ * @version (un numero di versione o una data)
+ * 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
+ */
+public class BlaBla
+{
+    // variabili d'istanza - sostituisci l'esempio che segue con il tuo
+    private int x;
+
+    /**
+     * Costruttore degli oggetti di classe  BlaBla
+     */
+    public BlaBla()
+    {
+        // inizializza le variabili d'istanza
+        x = 0;
+    }
+
+    /**
+     * Un esempio di metodo - aggiungi i tuoi commenti
+     * 
+     * @param  y   un parametro d'esempio per un metodo
+     * @return     la somma di x e y
+     */
+    public int sampleMethod(int y)
+    {
+        // metti qui il tuo codice
+        return x + y;
+    }
+}
